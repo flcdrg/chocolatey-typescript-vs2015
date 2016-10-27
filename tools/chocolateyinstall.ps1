2520cb78f1ceecf122b666046c9711a3a22f7cc2
David Gardiner <david@gardiner.net.au>
2016-10-27 10:37:43 +1030

    2.0.3.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'typescript-vs2015'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/TS2.0.2-TS-release20-nightly-20160828.1/TypeScript_Dev14Full.exe'
+$url        = 'https://download.microsoft.com/download/6/D/8/6D8381B0-03C1-4BD2-AE65-30FF0A4C62DA/TS2.0.3-TS-release20-nightly-20160921.1/TypeScript_Dev14Full.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -13,8 +13,8 @@ $packageArgs = @{
   silentArgs   = "/quiet /norestart /log `"$env:TEMP\chocolatey\$($packageName)\$($packageName).Install.log`""
   validExitCodes= @(0)
 
-  softwareName  = 'TypeScript Tools for Microsoft Visual Studio 2015 2.0.2.0 Beta'
-  checksum      = '90F26031DAEE276C08E485ED6BC2B3B7D6C8E7F910E83E6A2CBB9F20B770B423'
+  softwareName  = 'TypeScript Tools for Microsoft Visual Studio 2015 2.0.3.0'
+  checksum      = 'ABC646E25E4C218705548833C77EA850D06AB10E59C6B4B564F3BD8D0A2855F7'
   checksumType  = 'sha256'
 }
 
